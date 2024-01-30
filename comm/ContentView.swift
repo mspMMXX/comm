2904b86fac245cd42366074f6e41cd0962e13ed1
Markus <81870702+mspMMXX@users.noreply.github.com>
2024-01-30 14:52:28 +0100

    Version 0.0.0 - Start - ChatTF and Button

--- comm/ContentView.swift
+++ comm/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: - Properties
+    @State private var inputText: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            HStack {
+                TextField("Text", text: $inputText)
+                    .padding(5)
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(Color.black, lineWidth: 1))
+                Button(action: {
+                    print("Output")
+                    inputText = ""
+                }, label: {
+                    Image(systemName: "paperplane.circle.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                })
+            }
+            .padding(15)
         }
-        .padding()
     }
 }
 
