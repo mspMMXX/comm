ce2ed4141943a776bf8d7b4eb8c4a20ca01057b6
Markus <81870702+mspMMXX@users.noreply.github.com>
2024-01-30 14:11:00 +0100

    Initial Commit

---
+++ comm/commApp.swift
@@ -0,0 +1,17 @@
+//
+//  commApp.swift
+//  comm
+//
+//  Created by Markus Platter on 30.01.24.
+//
+
+import SwiftUI
+
+@main
+struct commApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
